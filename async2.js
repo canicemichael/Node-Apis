53bc84cdc872eb6c4bb649f6bd0773f795f5f5be
canicemichael <canicemichael@gmail.com>
2023-04-15 11:24:11 +0100

    chore: success implementation of asyncStorage

--- async2.js
+++ async2.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { AsyncLocalStorage } from "node:async_hooks";
-import uuid from "uuid/v4";
+import { uuid } from "uuidv4";
 
 const asyncLocalStorage = new AsyncLocalStorage();
 
 const requestIdMiddleware = (req, res, next) => {
-    asyncLocalStorage.run(new Map(), () => {
-        asyncLocalStorage.getStore().set("requestId", uuid());
-        next();
-    });
+  asyncLocalStorage.run(new Map(), () => {
+    asyncLocalStorage.getStore().set("requestId", uuid());
+    next();
+  });
 };
 
 const app = express();
@@ -16,9 +16,9 @@ const app = express();
 app.use(requestIdMiddleware);
 
 app.get("/", (req, res) => {
-    const id = asyncLocalStorage.getStore().get("requestId");
-    console.log(`[${id}] request received`);
-    res.send("It works!");
+  const id = asyncLocalStorage.getStore().get("requestId");
+  console.log(`[${id}] request received`);
+  res.send(`[${id}] request received  so it works`);
 });
 
 const port = process.env.PORT || 3000;
