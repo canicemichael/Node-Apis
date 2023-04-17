fa12e96d5df004bb7e3e590f6fdfeb50cb46cce4
canicemichael <canicemichael@gmail.com>
2023-04-17 03:23:06 +0100

    add: url and buffer node api concept understanding

---
+++ url.js
@@ -0,0 +1,10 @@
+import url from "node:url";
+
+const myURL = new URL("https://user:pass@sub.example.com:8080/p/a/t/h?query=string&abc=xyz#hash");
+console.log(myURL.searchParams)
+
+console.log(myURL.searchParams.get("abc"));
+
+myURL.searchParams.append('kyc', "bykugan");
+console.log(myURL.href);
+console.log(myURL.searchParams.get("kyc"));
