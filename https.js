c3cc59401d4a5fb4e5fc6bdb3253d54069667464
canicemichael <canicemichael@gmail.com>
2023-04-20 10:32:06 +0100

    chore: implement http get core method

--- https.js
+++ https.js
@@ -1,7 +1,97 @@
 import https from "https";
 
-let request = https.get(
-  "https://jsonplaceholder.typicode.com/users?_limit=2",
+// 1.
+// let request = https.get(
+//   "https://jsonplaceholder.typicode.com/users?_limit=2",
+//   (res) => {
+//     if (res.statusCode !== 200) {
+//       console.error(
+//         `Did not get an OK from the server. Code: ${res.statusCode}`
+//       );
+//       res.resume();
+//       return;
+//     }
+
+//     let data = "";
+
+//     res.on("data", (chunk) => {
+//       data += chunk;
+//       console.log(chunk.toString());
+//     });
+
+//     res.on("close", () => {
+//       console.log("Retrieved all data");
+//       console.log(JSON.parse(data));
+//     });
+
+//   }
+// );
+
+// request.on("error", (err) => {
+//   console.error(
+//     `Encountered an error trying to make a request: ${err.message}`
+//   );
+// });
+
+// console.log(request);
+
+// 2.
+// const options = {
+//   method: "GET",
+// };
+
+// let request = https.request(
+//   "https://jsonplaceholder.typicode.com/users?_limit=2",
+//   options,
+//   (res) => {
+//     if (res.statusCode !== 200) {
+//       console.error(
+//         `Did not get an OK from the server. Code: ${res.statusCode}`
+//       );
+//       res.resume();
+//       return;
+//     }
+
+//     let data = "";
+
+//     res.on("data", (chunk) => {
+//       data += chunk;
+//       console.log(chunk.toString());
+//     });
+
+//     res.on("close", () => {
+//       console.log("Retrieved all data");
+//       console.log(JSON.parse(data));
+//     });
+//   }
+// );
+
+// // You also call the end() method of the request variable.
+// // This is an important method that must be called when using
+// // the request() function. It completes the request, allowing
+// // it to be sent. If you don’t call it, the program will never
+// // complete, as Node.js will think you still have data to add
+// // to the request.
+// request.end();
+
+// request.on("error", (err) => {
+//   console.error(
+//     `Encountered an error trying to make a request: ${err.message}`
+//   );
+// });
+
+// 3.
+const options = {
+  host: 'jsonplaceholder.typicode.com',
+  path: "/users?_limit=2",
+  method: "GET",
+  headers: {
+    'Accept': 'application/json'
+  }
+};
+
+let request = https.request(
+  options,
   (res) => {
     if (res.statusCode !== 200) {
       console.error(
@@ -11,17 +101,29 @@ let request = https.get(
       return;
     }
 
-    let data = '';
+    let data = "";
 
-    res.on('data', (chunk) => {
-        data += chunk;
+    res.on("data", (chunk) => {
+      data += chunk;
     });
 
-    res.on('close', () => {
-        console.log('Retrieved all data');
-        console.log(JSON.parse(data));
+    res.on("close", () => {
+      console.log("Retrieved all data");
+      console.log(JSON.parse(data));
     });
   }
 );
 
-// console.log(request);
+// You also call the end() method of the request variable.
+// This is an important method that must be called when using
+// the request() function. It completes the request, allowing
+// it to be sent. If you don’t call it, the program will never
+// complete, as Node.js will think you still have data to add
+// to the request.
+request.end();
+
+request.on("error", (err) => {
+  console.error(
+    `Encountered an error trying to make a request: ${err.message}`
+  );
+});
